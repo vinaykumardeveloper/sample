50a48b17af01be228f27302b7abcb8b4f00b720e
vinaykumardarna2468 <137882200+vinaykumardarna2468@users.noreply.github.com>
2024-08-30 06:45:40 +0000

    thank you is added to confrmation page

--- appointment/src/AppointmentConfirmation.js
+++ appointment/src/AppointmentConfirmation.js
@@ -15,6 +15,7 @@ const AppointmentConfirmation = () => {
       <p>Age: {appointment.age}</p>
       <p>Date: {appointment.date}</p>
       <p>Time: {appointment.time}</p>
+      <p>Thank you</p>
     </div>
   );
 };
